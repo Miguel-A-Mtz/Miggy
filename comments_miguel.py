c4db4546e78811d11ac9c61ab03d4929f0c123c1
Miguel-A-Mtz <mmartinezx707@gmail.com>
2024-07-25 00:55:35 -0500

    Add files via upload

---
+++ comments_miguel.py
@@ -0,0 +1,19 @@
+# Miguel Martinez
+# Introduction to programming with python
+# Script 2:
+# MIT Open Source License (see license.html)
+
+# Insert a triple-single-quote multi-line comment describes when to use this comment style
+""" This is a standard multi-line comment """
+
+# Insert a triple-double-quote multi-line comment that describes when to use this comment style
+"""  this type of multi-line comment is used with Python Objects """
+
+# 5) declare my_name, with value
+my_name = "miguel"
+
+# 6) name and print the value from the variable.
+print("My name is", my_name)
+
+# 7) Use the .format() syntax to create a single statement that prints label and value
+print("My name is {}".format(my_name))
