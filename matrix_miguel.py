911eae110b9e4a1a479541bd309d7aecbb428524
Miguel-A-Mtz <mmartinezx707@gmail.com>
2024-07-25 01:00:15 -0500

    Add files via upload

---
+++ matrix_miguel.py
@@ -0,0 +1,42 @@
+ # Miguel Martinez
+ # CIS 135 Summer Python Programming
+ # Lab 13: Matrix
+
+
+ def main ():
+     print("\nWelcome to Lab 13 Multi-Dimensional Arrays")
+
+     r1 = ('X','R','S')
+     r2 = ('L','Y','T')
+     r3 = ('M','N','Z')
+     c1 = ('X','L','M')
+     c2 = ('R','Y','N')
+     c3 = ('S','T','Z')
+
+    print(f'{r1[0]}
+      
+      for each, value in enumerate(r1, r2, r3):
+            print(f'{value}')
+    all_rows = [r1, r2, r3]
+    for each, value in enumerate(all_rows):
+        print(f'{value[0]}')
+        return
+
+
+    print(f'\nPrinting Column Values with a loop')
+    all_cols = [c1, c2, c3]
+    for each, value in enumerate(all_cols):
+        print(f'\t{value[0]}
+        clone_list = list()
+        clone_list = all_rows
+
+
+
+
+    clone_loop = list())
+    for each in all_rows:
+        clone_loop.appen(each
+    print(f'clone_loop is {clone_loop}')
+
+    # Create a cloned list usinh list comprehension
+    cloned_comp +
